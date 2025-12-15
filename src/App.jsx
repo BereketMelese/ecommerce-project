dfc8c5126afda62b7a3e4717ce34333e7ccabd73
BereketMelese <bereketmeles00@gmail.com>
2025-12-15 15:30:50 +0300

    feat: add react-router for routing and update dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import HomePage from './pages/HomePage'
+import { Route, Routes } from "react-router";
+import HomePage from "./pages/HomePage";
 
 function App() {
-
   return (
-    <>
-      <HomePage />
-    </>
-  )
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
