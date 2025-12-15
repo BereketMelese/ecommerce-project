5306d78dc7b054e8c95133601f4ba74f69b73dfc
BereketMelese <bereketmeles00@gmail.com>
2025-12-15 17:41:38 +0300

    feat: update Header component to display total cart quantity and fetch cart items in HomePage

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,11 @@ import mobileLogo from "../assets/images/mobile-logo-white.png";
 
 import "./Header.css";
 
-function Header() {
+function Header({ cart }) {
+  let totalQuantity = 0;
+  cart.forEach((cartItem) => {
+    totalQuantity += cartItem.quantity;
+  });
   return (
     <div className="header">
       <div className="left-section">
@@ -29,7 +33,7 @@ function Header() {
 
         <NavLink className="cart-link header-link" to="/checkout">
           <img className="cart-icon" src="images/icons/cart-icon.png" />
-          <div className="cart-quantity">3</div>
+          <div className="cart-quantity">{totalQuantity}</div>
           <div className="cart-text">Cart</div>
         </NavLink>
       </div>
