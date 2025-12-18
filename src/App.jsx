266082b42b68090b43bfde10a278e8cd2e8e1213
BereketMelese <bereketmeles00@gmail.com>
2025-12-18 11:47:27 +0300

    feat: refactor HomePage and CheckoutPage components; introduce OrderSummary and PaymentSummary components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { Route, Routes } from "react-router";
 import axios from "axios";
-import HomePage from "./pages/HomePage";
+import HomePage from "./pages/home/HomePage";
 import CheckoutPage from "./pages/checkout/CheckoutPage";
 import OrdersPage from "./pages/OrdersPage";
 import TrackingPage from "./pages/TrackingPage";
