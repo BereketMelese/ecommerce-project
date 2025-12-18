07f87419a1c0978ea57031918e9a044d841a435e
BereketMelese <bereketmeles00@gmail.com>
2025-12-18 13:17:41 +0300

    feat: refactor data fetching in App, CheckoutPage, and OrdersPage components to use async/await for improved readability

--- src/pages/orders/OrdersPage.jsx
+++ src/pages/orders/OrdersPage.jsx
@@ -7,9 +7,12 @@ function OrdersPage({ cart }) {
   const [orders, setOrders] = useState([]);
 
   useEffect(() => {
-    axios.get("/api/orders?expand=products").then((response) => {
+    const fetchOrderData = async () => {
+      const response = await axios.get("/api/orders?expand=products");
       setOrders(response.data);
-    });
+    };
+
+    fetchOrderData();
   }, []);
 
   return (
